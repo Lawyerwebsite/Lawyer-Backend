434a718c1fbab01b7236ea5440a3abd28dba9c15
Abdul2102 <114062056+Abdul2102@users.noreply.github.com>
2025-02-13 17:51:19 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ const path = require("path")
 const cors = require("cors");
 const app = express();
 
-app.use(cors({
-  origin: "https://lawlinkuser.netlify.app/",
-  methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"]
-}));
+app.use(cors("*"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
